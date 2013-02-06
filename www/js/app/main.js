82bedc6ef16b798563aea0a249f49512f4bb56b2
C. Scott Ananian <cscott@cscott.net>
2013-02-06 12:19:52 -0500

    Hook up backbone router.

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -4,6 +4,7 @@ define(function (require) {
     'use strict';
 
     var $ = require('jquery');
+    var Backbone = require('backbone');
 
     // Dependencies that do not have an export of their own, just attach
     // to other objects, like jQuery. These are just used in the example
@@ -11,6 +12,26 @@ define(function (require) {
     // displays.
     require('bootstrap/modal');
     require('bootstrap/transition');
+
+    // Page routing
+    var WikiRouter = Backbone.Router.extend({
+        routes: {
+            "about": "showAbout",
+            "wiki/*title": "wikiPage",
+            "*other": "defaultPage"
+        },
+        showAbout: function() {
+            this.navigate("wiki/Nell:About", true);
+        },
+        defaultPage: function() {
+            this.navigate("wiki/Nell:Home", true);
+        },
+        wikiPage: function(title) {
+            console.log('wiki',title,'opened');
+        }
+    });
+    var myRouter = new WikiRouter();
+    Backbone.history.start();
 
     // Wait for the DOM to be ready before showing the network and appCache
     // state.
