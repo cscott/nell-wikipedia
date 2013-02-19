88f68e0d321358d858b788302a6953c09b8470e7
C. Scott Ananian <cscott@cscott.net>
2013-02-18 19:02:15 -0500

    Rewrite 'test-parsoid' tool to do article conversion in parallel.
    Actually seems to make all the HTTP requests at once at the beginning,
    which is a little more parallelism than expected!

--- tools/wiki/test-parsoid.js
+++ tools/wiki/test-parsoid.js
@@ -5,6 +5,7 @@ var requirejs = require('requirejs'), pjson = require('../../package.json');
 requirejs(['commander', 'async', 'fs', 'http'], function(program, async, fs, http) {
     "strict mode";
     var PARSOID_HOST = 'nell-parsoid.aws.af.cm';
+    var DEBUG = false;
 
     // main routine
     program
@@ -26,17 +27,21 @@ requirejs(['commander', 'async', 'fs', 'http'], function(program, async, fs, htt
         map(function(line) {
             return line.split(/\s+/)[0];
         }).filter(function(title) { return !!title; });
-    //article_list.forEach(function(title) { console.log(JSON.stringify(title)); });
 
     var output = fs.createWriteStream(program.output, {encoding:'utf8'});
 
     var fetchOne = function(title, callback) {
         var page_url = 'http://'+PARSOID_HOST+'/'+program.lang+'/'+title;
         var page_contents = '';
-        console.log(title, 'requested');
+        if (DEBUG) console.log(title, 'requested');
         http.get(page_url, function(res) {
-            console.assert(res.statusCode === 200);
-            console.log(title, 'response begun');
+            if (res.statusCode !== 200) {
+                console.error("HTTP ERROR CODE", res.statusCode, title);
+                callback(null, {title:title, contents:"<h1>"+res.statusCode+"</h1>"});
+                return;
+            }
+            console.assert(res.statusCode === 200, res.statusCode+' '+title);
+            if (DEBUG) console.log(title, 'response begun');
             res.setEncoding('utf8');
             res.on('data', function(chunk) {
                 page_contents += chunk;
@@ -44,7 +49,7 @@ requirejs(['commander', 'async', 'fs', 'http'], function(program, async, fs, htt
                 callback(null, {title:title, contents:page_contents});
             });
         }).on('error', function(e) {
-            console.error("HTTP ERROR", e.message);
+            console.error("HTTP EXCEPTION", e.message);
             callback(e);
         });
     };
@@ -53,22 +58,25 @@ requirejs(['commander', 'async', 'fs', 'http'], function(program, async, fs, htt
         var buffer = new Buffer(page.contents, 'utf8');
         output.write(buffer.length+'\n');
         output.write(buffer);
-        console.log(page.title, 'written');
+        if (DEBUG) console.log(page.title, 'written');
         callback(null, 'done');
     };
-    // create 'auto'
-    tasks = article_list.map(function(title) {
-        return function(callback) {
-            async.waterfall([function(cb1) {cb1(null, title);},
-                             fetchOne, writeOne], callback);
+    // create 'auto' dependency object
+    var previous = null, autodeps = {};
+    article_list.forEach(function(title) {
+        autodeps["FETCH "+title] = function(callback) {
+            fetchOne(title, callback);
         };
+        autodeps["WRITE "+title] = ["FETCH "+title, function(callback, results){
+            var page = results["FETCH "+title];
+            writeOne(page, callback);
+        }];
+        if (previous) {
+            autodeps["WRITE "+title].unshift("WRITE "+previous);
+        }
+        previous = title;
     });
-
-    //tasks[0](function(e,r) { console.log(e,r); output.destroySoon(); });
-    //fetchOne(article_list[0], function(x){console.log(x);});
-
-    async.series(tasks, function(e, r) {
-        console.log(e, r);
+    async.auto(autodeps, function() {
         output.destroySoon();
     });
 });
