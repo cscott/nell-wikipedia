d155917d32ae3b1c90ba6bb751db08d2c289aec6
C. Scott Ananian <cscott@cscott.net>
2014-03-30 11:43:02 -0400

    Update configuration to point to public WMF Parsoid service.
    The _wikitext endpoint is not public, though, so we still need to use
    our private appfog server for our local pages.  Probably should be
    changed by preprocessing our wikitext sources through wmflabs
    _wikitext endpoint and storing the DOM sources.

--- www/js/app/config.js
+++ www/js/app/config.js
@@ -2,15 +2,17 @@
 define([], function() {
     var Config = {
         // parsoid helper
-        parsoid_host: 'nell-parsoid.aws.af.cm',
-        parsoid_convert_mw_url: 'http://{{parsoid_host}}/_wikitext/',
+        parsoid_host: 'parsoid-lb.eqiad.wikimedia.org',
+        // official mw service doesn't have a public _wikitext endpoint
+        //parsoid_convert_mw_url: 'http://{{parsoid_host}}/_wikitext/',
+        parsoid_convert_mw_url: 'http://nell-parsoid.aws.af.cm/_wikitext/',
         parsoid_fetch_url: 'http://{{parsoid_host}}/{{lang}}/{{title}}',
         // whether to use hashes or slashes
         path_root: '#!/', /* '#!/' or '#' or '' */
 
         // language to use for wikipedia content.
         // (ie, <lang>.wikipedia.org)
-        lang: 'simple',
+        lang: 'simplewiki',
 
         // xxx: these should be localized?
         // portal page
